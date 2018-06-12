8f8b712966cc7bfa7ea03058373c6334b29d958e
BuildTools <unconfigured@null.spigotmc.org>
2018-06-11 18:18:16 -0700

    Fixing Issue

--- public/js/checkout.js
+++ public/js/checkout.js
@@ -1,4 +1,4 @@
-var stripe = Stripe('***REMOVED***');
+var stripe = Stripe('pk_live_8NWtBaZPTXhOlgFotbdEOOd3');
 var elements = stripe.elements();
 // Custom styling can be passed to options when creating an Element.
 var style = {
